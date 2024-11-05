93aa23ffbd7f98234cab2742a8fa085b9317017e
Myraa <boddu.prasad@gmail.com>
2024-11-04 20:14:50 -0600

    added formatting

--- src/train.py
+++ src/train.py
@@ -3,11 +3,15 @@ import mlflow
 import tensorflow as tf
 
 def train_model():
+
+
     # Load and preprocess data, define model, etc.
     # ...
 
     # Start MLflow run
     with mlflow.start_run():
+
+        
         # Log parameters, metrics, model, etc.
         # ...
         pass
